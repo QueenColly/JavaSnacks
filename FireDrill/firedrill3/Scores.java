1bcb3ca87976b9035eff04d7362629b7b7e9b225
QueenColly <colleteumeh0@gmail.com>
2026-05-04 18:50:13 +0100

    first commit

---
+++ FireDrill/firedrill3/Scores.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class TaskOne{
+public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+  
+//    int score = input.nextInt();
+     int [] list =new int[10];
+      System.out.println("Enter  scores: ");
+    
+//
+//     for(int number = 0; number < list.length; number ++){
+//        list[number] = input.nextInt();                                                             
+//
+
+    for(int number = 0; number < list.length; number ++){
+        System.out.print("Enter a score "+number+ " :");
+       list[number] = input.nextInt(); 
+//     System.out.println(score);
+     System.out.println(list[number]+ " ");
+
+    }
+    
+   
+
+    for(int number = 0; number < list.length; number++){
+       
+             System.out.println(list[number]+ " ");
+  
+  
+}
+   }
+}
+
+    
+
---
+++ FireDrill/firedrill3/TaskFive.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+public class TaskFive {
+public static void main(String[] args){
+//}
+
+int[] numbers = {2,1,5,7,8};
+System.out.println(Arrays.toString(getOddArray(numbers)));
+}
+
+
+    public static int[] getOddArray(int [] numbers){
+//    int count =0;
+    int [] result =new int[numbers.length];
+
+    for(int index = 0; index < numbers.length; index++){
+        if(numbers[index] % 2 !=0){
+
+         result[index]= numbers[index];
+
+        
+
+        }       
+
+    
+    }
+  return result;
+
+    }
+}
