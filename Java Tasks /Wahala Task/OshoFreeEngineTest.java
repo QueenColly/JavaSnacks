c5bd9ba1e66a6d36ba49da4afdb1d1ecf99bc59a
QueenColly <colleteumeh0@gmail.com>
2026-05-13 03:12:01 +0100

    first commit

--- Java Tasks /Wahala Task/OshoFreeEngineTest.java
+++ Java Tasks /Wahala Task/OshoFreeEngineTest.java
@@ -1,1 +1,58 @@
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OshoFreeEngineTest{
+
+    @Test
+    public void testThatBIGBOY20_promoCodeDiscountWorks(){
+//        given
+        double price = 15000;
+//        actual
+        double result = OshoFreePromoEngine.getFinalPrice(price, "BIGBOY20");
+//        expected
+        double finalPrice = 12000;
+        assertEquals(finalPrice, result);
+        
+    }
+   @Test
+    public void testThatSTARTER10_promoCodeDiscountWorks(){
+//        given
+        double price = 13000;
+//        actual
+        double result = OshoFreePromoEngine.getFinalPrice(price,"STARTER10");
+//        expected
+        double finalPrice = 11700;
+        assertEquals(finalPrice, result);
+        
+    }
+ @Test
+    public void testThatOSHOFREE_promoCodeDiscountWorks(){
+//        given
+        double price = 30000;
+//        actual
+        double result = OshoFreePromoEngine.getFinalPrice(price,"OSHOFREE35");
+//        expected
+        double finalPrice = 19500;
+        assertEquals(finalPrice, result);
+        
+    }
+
+@Test
+    public void testThatNOTAPPLICABLE_promoCodeDiscountWorks(){
+//        given
+        double price = 3000;
+//        actual
+        double result = OshoFreePromoEngine.getFinalPrice(price,"NOT APPLICABLE");
+//        expected
+        double finalPrice = 3000;
+        assertEquals(finalPrice, result);
+        
+    }
+
+
+
+
+    
+
+}
--- Java Tasks /Wahala Task/OshoFreePromoEngine.java
+++ Java Tasks /Wahala Task/OshoFreePromoEngine.java
@@ -1,1 +1,37 @@
+public class OshoFreePromoEngine{
 
+    public static void main(String[] args){
+        double result = getFinalPrice(3000,"NOT APPLICABLE");
+        System.out.println(result);
+    }   
+
+    public static double getFinalPrice(double price, String promoCode){
+        double finalPrice = 0;
+        double promoValue = 0.0;
+
+        if(price < 5000 && promoCode.equals("NOT APPLICABLE")){
+            promoValue = 0.0;
+        }
+        if (price >=5000 && price <= 14999 && promoCode.equals("STARTER10")){
+            promoValue = 0.1;
+            System.out.println(promoValue);
+        }
+        if(price >= 15000 && price <= 29999 && promoCode.equals("BIGBOY20")){
+            promoValue = 0.2;
+            System.out.println(promoValue);
+        }
+
+        if(price>=30000 && promoCode.equals("OSHOFREE35")){
+            promoValue = 0.35;
+            System.out.println(promoValue);
+        }
+
+        double discountAmount = price * promoValue;
+        finalPrice = price - discountAmount;
+        
+        return finalPrice;
+
+    }
+
+
+}
